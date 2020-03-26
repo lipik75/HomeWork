5362bbd8ea5496ec80a5909c47838e3565be742c
Ihar Sivukha <62198481+lipik75@users.noreply.github.com>
2020-03-26 20:08:10 +0300

    Add files via upload

---
+++ ДЗ-3/Main.java
@@ -0,0 +1,9 @@
+package edu.academy;
+
+public class Main {
+    static String abc = "Hello";
+
+    public static void main(String[] args) {
+        System.out.println(abc.substring(0, 3));
+    }
+}
